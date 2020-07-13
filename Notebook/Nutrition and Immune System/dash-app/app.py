abdd8eed35f297ca08a03fb4440af879009fbcfc
Md Jonybul islam <MD_Jonybul.Islam@dfki.de>
2020-07-13 22:24:04 +0200

    Full app.

--- Notebook/Nutrition and Immune System/dash-app/app.py
+++ Notebook/Nutrition and Immune System/dash-app/app.py
@@ -46,7 +46,7 @@ app.layout = html.Div(children=[
                 id="Radio_differentFoods",
                 options=[
                     {'label': hp.column_names[i], 'value': hp.column_names[i]}
-                    for i in range(1, 24)
+                    for i in range(1,24)
                 ],
                 value='Alcoholic Beverages',
                 labelStyle={'display': 'inline-block',
@@ -82,20 +82,84 @@ app.layout = html.Div(children=[
                 dcc.Graph(
                     id="graph_tab_1_2"
                 ),
-            ])
+            ]),
+            html.Div([
+                    html.P("Food Type:",
+                           style={'display': 'inline-block',
+                                  'margin-left': '35px',
+                                  'margin-top': '50px',
+                                  'fontSize': 20})
+            ]),
+            html.Div(
+                [
+                dcc.RadioItems(
+                        id="radio_pie",
+                        options=[
+                            {'label': 'Protein', 'value': 'Protine'},
+                            {'label': 'Fat', 'value': 'Fat'},
+                            {'label': 'KCal', 'value': 'KCal'},
+                            {'label': 'Quantity', 'value': 'Quantity'}
+                        ],
+                        value='Protine',
+                        labelStyle={'display': 'inline-block',
+                                    'margin-left': '5px',
+                                    'fontSize': 15}
+                    )],className="pretty_container six columns",
+            ),
+            html.Div([
+                    html.Div(
+                        [
+                            html.P("Select 1st Country:", className="control_label_1"),
+                            dcc.Dropdown(
+                                id="Tab_1_dropdown_1",
+                                options=[
+                                    {"label": country, "value": country}
+                                    for country in hp.Fat_Supply_Quantity_Data['Country']
+                                ],
+                                value='Germany',
+                                clearable=False,
+                                multi=False
+                            )
 
+                        ],
+                        className="pretty_container six columns",
+                    ),
+                    html.Div(
+                        [
+                            html.P("Select 2nd Country:", className="control_label_2"),
+                            dcc.Dropdown(
+                                id="Tab_1_dropdown_2",
+                                options=[
+                                    {"label": country, "value": country}
+                                    for country in hp.Fat_Supply_Quantity_Data['Country']
+                                ],
+                                value='Germany',
+                                clearable=False,
+                                multi=False
+                            )
+
+                        ],
+                        className="pretty_container six columns",
+                    ),
+                ],
+                style={
+                    "margin-top": "35px",
+                    "margin-left": "10px"
+                }),
+            html.Div([
+                html.Div([
+                    dcc.Graph(
+                        id="Tab_1_graph_3"
+                    )], className="pretty_container six columns",
+                ),
+                html.Div([
+                    dcc.Graph(
+                        id="Tab_1_graph_4"
+                    )], className="pretty_container six columns",
+                )])
         ]),
         dcc.Tab(label='Tab two', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [1, 4, 1],
-                         'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [1, 2, 3],
-                         'type': 'bar', 'name': u'Montréal'},
-                    ]
-                }
-            )
+
         ]),
         dcc.Tab(label='Tab three', children=[
             dcc.Graph(
@@ -123,6 +187,7 @@ def update_figure_scatter_plot(Radio_foodType, Radio_differentFoods, Radio_Outpu
     data = hp.scatter_plot_tab_1(Radio_foodType, Radio_differentFoods, Radio_Output)
     return data
 
+
 # def update_figure Bar
 @app.callback(
     Output('graph_tab_1_2', 'figure'),
@@ -130,6 +195,25 @@ def update_figure_scatter_plot(Radio_foodType, Radio_differentFoods, Radio_Outpu
 def update_figure_box_plot(Radio_foodType):
     data = hp.box_plot_tab_1(Radio_foodType)
     return data
+
+
+@app.callback(
+    Output('Tab_1_graph_3', 'figure'),
+    [Input('radio_pie','value'),
+    Input('Tab_1_dropdown_1', 'value')])
+def pie_chart_1(radio,dropDown):
+    data=hp.pie_chart_1_for_app(dropDown,radio)
+    return data
+
+@app.callback(
+    Output('Tab_1_graph_4', 'figure'),
+    [Input('radio_pie','value'),
+    Input('Tab_1_dropdown_2', 'value')])
+def pie_chart_2(radio,dropDown):
+    data=hp.pie_chart_1_for_app(dropDown,radio)
+    return data
+
+
 
 
 if __name__ == '__main__':
